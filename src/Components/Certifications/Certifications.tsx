047e5b8304d2262631f4142a676eee8179fbcd8c
Gabriel Mendes <guiadeviagens@outlook.com.br>
2023-10-24 00:14:48 -0300

    Add more exp

--- src/Components/Certifications/Certifications.tsx
+++ src/Components/Certifications/Certifications.tsx
@@ -9,8 +9,11 @@ const Certifications = (curriculum: Curriculum, darkMode: boolean) => {
                 {curriculum.certifications.map((cert, index) => {
                     if (cert.image === "" || cert.link === "") return null;
                     return (
+                        !cert.image.includes("csharp") ?
                         <a title={cert.name} key={index} target="_blank" rel="noreferrer" href={cert.link}><img alt='certificate' width={80} height={80} src={cert.image} /></a>
-                    )
+                        :
+                        <a title={cert.name} key={index} target="_blank" rel="noreferrer" href={cert.link}><svg  className='csharp' width={80} height={80} /></a>
+                        )
                 })}
             </div>
         </div>
--- src/Components/Experiences/Experiences.tsx
+++ src/Components/Experiences/Experiences.tsx
@@ -5,11 +5,16 @@ import './Experiences.css';
 const Experiences = (curriculum: Curriculum, darkMode: boolean) => {
     return (
         <section className={darkMode ? 'darkmode-experiences' : 'experiences'}>
+            {WhoAmI(curriculum, darkMode)}
             <h3>Experiences</h3>
             {curriculum.experiences.map((expe, index) => {
                 return (
                     <div key={index}>
+                        <div style={{display: 'flex', alignItems: 'center'}}>
+                        <svg className={'logo-' + expe.enterprise.split(" ")[0].toLowerCase()} />
                         <h3 title={expe.enterprise}>{expe.enterprise + " - " + expe.role}</h3>
+                        </div>
+
                         <p>{expe.startedMonth + "/" + expe.startedYear} {expe.stillWorking ? " - Current" : " - " + expe.endMonth + "/" + expe.endYear}</p>
 
                         <span>{expe.description}</span>
@@ -17,7 +22,6 @@ const Experiences = (curriculum: Curriculum, darkMode: boolean) => {
                     </div>
                 );
             })}
-            {WhoAmI(curriculum, darkMode)}
         </section>
     )
 }
